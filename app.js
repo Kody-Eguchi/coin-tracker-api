35bf11fa25c38ee699cf3cfc8ffe0e7e9da2a0cd
Koji Eguchi <koji.eguchi@hotmail.com>
2023-09-06 19:43:34 -0400

    Implement a login function to generate a token

--- app.js
+++ app.js
@@ -1,21 +1,39 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
+const cors = require("cors");
 
 //IMPORT PRISMA
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
+
+// CORS
+const allowedOrigins = ["http://localhost:3000"];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
 //MIDDLEWARE
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors(corsOptions));
 
 const users = require("./routes/users");
+const login = require("./routes/login");
 
 app.get("/", (req, res) => {
   res.send("it works");
 });
 
 app.use("/api/users", users(prisma));
+app.use("/api/login", login(prisma));
 
 app.listen(PORT, () => {
   console.log(`Listening to port ${PORT}`);
---
+++ routes/login.js
@@ -0,0 +1,34 @@
+const router = require("express").Router();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+module.exports = (prisma) => {
+  // RETURN USER INFORMATION UPON AUTHENTICATION
+  router.post("/", async (req, res) => {
+    const { email, password } = req.body;
+    // FIND THE USER BY EMAIL
+    try {
+      const user = await prisma.user.findFirst({
+        where: {
+          email,
+        },
+      });
+      console.log(user, "🚨");
+      // if (!user || !(await bcrypt.compare(password, user.password))) {
+      //   return res.status(401).json({ error: "Invalid credentials" });
+      // }
+
+      const token = jwt.sign({ userId: user.id }, "your-secret-key", {
+        expiresIn: "1h", // Token expiration time
+      });
+      console.log(token, "🔑");
+      // Respond with the token
+      res.status(200).json({ token });
+    } catch (error) {
+      console.error("Error", error);
+      res.status(500).json({ error: "Server error" });
+    }
+  });
+
+  return router;
+};
