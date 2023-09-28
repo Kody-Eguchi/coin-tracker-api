af8d100f20aac8d3c9ab63a69520e7e4b0941085
Koji Eguchi <koji.eguchi@hotmail.com>
2023-09-27 20:26:23 -0400

    Add create operation to create a new goal

--- routes/goal.js
+++ routes/goal.js
@@ -35,5 +35,44 @@ module.exports = (prisma) => {
     });
   });
 
+  // CREATE A NEW GOAL
+  router.post("/", async (req, res) => {
+    const token = req.headers.authorization?.split(" ")[1];
+
+    if (!token) {
+      return res.status(401).json({ error: "Missing token" });
+    }
+
+    jwt.verify(token, secretKey, async (err, decoded) => {
+      if (err) {
+        // VERIFICATION ERROR HANDLING
+        return res.status(401).json({ error: "Invalid token" });
+      }
+
+      // EXTRACT THE USERID FROM THE DECODED TOKEN
+      const userId = decoded.userId;
+
+      try {
+        const { description, goal_amount, target_date } = req.body;
+        const midnightTargetDate = new Date(target_date);
+        midnightTargetDate.setUTCHours(0, 0, 0, 0);
+        const goal = await prisma.goal.create({
+          data: {
+            creation_date: new Date(),
+            description,
+            goal_amount,
+            target_date: midnightTargetDate,
+            user_id: userId,
+          },
+        });
+
+        res.status(201).json(goal);
+      } catch (error) {
+        console.error("Error:", error);
+        res.status(500).json({ error: "Failed to create goal" });
+      }
+    });
+  });
+
   return router;
 };
