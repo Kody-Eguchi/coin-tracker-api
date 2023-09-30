0d76d2f28188a389058fc922cc7b93d323ffd6e3
Koji Eguchi <koji.eguchi@hotmail.com>
2023-09-30 16:58:03 -0400

    Add delete operation for Goal controller

--- routes/goal.js
+++ routes/goal.js
@@ -74,5 +74,49 @@ module.exports = (prisma) => {
     });
   });
 
+  // DELETE A GOAL BY ID
+  router.delete("/delete", async (req, res) => {
+    const token = req.headers.authorization?.split(" ")[1];
+    if (!token) {
+      return res.status(401).json({ error: "Missing token" });
+    }
+
+    //VERIFY AND DECODE THE TOKEN
+    jwt.verify(token, secretKey, async (err, decoded) => {
+      if (err) {
+        // VERIFICATION ERROR HANDLING
+        return res.status(401).json({ error: "Invalid token" });
+      }
+
+      // EXTRACT THE USERID FROM THE DECODED TOKEN
+      const userId = decoded.userId;
+
+      try {
+        const { goal_id } = req.body;
+
+        const targetGoal = await prisma.goal.findUnique({
+          where: {
+            goal_id: goal_id,
+            user_id: userId,
+          },
+        });
+
+        if (!targetGoal) {
+          return res.status(404).json({ message: "Goal not found" });
+        }
+
+        await prisma.goal.delete({
+          where: {
+            transaction_id: transaction_id,
+            user_id: userId,
+          },
+        });
+      } catch (error) {
+        console.error("Error deleting goal:", error);
+        res.status(500).json({ message: "Internal server error" });
+      }
+    });
+  });
+
   return router;
 };
